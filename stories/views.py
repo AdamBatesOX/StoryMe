dfa9c78cd97f8700357b4000ef663ad72ca1f532
LionelDeerhunter <adam@objectexperience.co.uk>
2020-06-23 11:12:01 +0100

    11 - Handling Dynamic Routing

--- stories/views.py
+++ stories/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+from .models import Story
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
     return HttpResponse("<h1>Microfiction Site<h1>")
 
 def story_detail_view(request, story_id,  *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {story_id}<h1>")
+    try:
+        obj = Story.objects.get(id=story_id)
+    except:
+        raise Http404
+    return HttpResponse(f"<h1>Hello {story_id} - {obj.content}<h1>")
